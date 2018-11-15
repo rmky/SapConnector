56ac5540516ca4732a6002d37bc8c42e6966360c
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-15 09:51:09 +0100

    FIX SapODataJsonUrlBuilder

--- QueryBuilders/SapODataJsonUrlBuilder.php
+++ QueryBuilders/SapODataJsonUrlBuilder.php
@@ -2,7 +2,6 @@
 namespace exface\SapConnector\QueryBuilders;
 
 use exface\UrlDataConnector\QueryBuilders\ODataJsonUrlBuilder;
-use exface\UrlDataConnector\Psr7DataQuery;
 
 /**
  * Query builder for SAP oData services in JSON format.
@@ -12,14 +11,5 @@ use exface\UrlDataConnector\Psr7DataQuery;
  */
 class SapODataJsonUrlBuilder extends ODataJsonUrlBuilder
 {
-    /**
-     * 
-     * {@inheritDoc}
-     * @see \exface\UrlDataConnector\QueryBuilders\ODataJsonUrlBuilder::buildPathToResponseRows()
-     */
-    protected function buildPathToResponseRows(Psr7DataQuery $query)
-    {
-        $customPath = $this->getMainObject()->getDataAddressProperty('response_data_path');
-        return $customPath ?? 'd/results';
-    }
+    
 }
