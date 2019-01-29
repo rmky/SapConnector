2f3521d60c51db72dd68b6a227f9ee4429863793
aka <andrej.kabachnik@salt-solutions.de>
2019-01-29 12:54:11 +0100

    FIX various issues with OpenSQL builder: subselects, COUNT(), large INs

--- QueryBuilders/SapOpenSqlBuilder.php
+++ QueryBuilders/SapOpenSqlBuilder.php
@@ -5,6 +5,11 @@ use exface\Core\QueryBuilders\MySqlBuilder;
 use exface\Core\CommonLogic\QueryBuilder\QueryPartSorter;
 use exface\Core\Interfaces\DataTypes\DataTypeInterface;
 use exface\Core\DataTypes\DateDataType;
+use exface\Core\Interfaces\Model\MetaAttributeInterface;
+use exface\Core\DataTypes\StringDataType;
+use exface\Core\DataTypes\AggregatorFunctionsDataType;
+use exface\Core\CommonLogic\QueryBuilder\QueryPartAttribute;
+use exface\Core\Interfaces\Model\AggregatorInterface;
 
 /**
  * SQL query builder for SAP OpenSQL
@@ -43,17 +48,24 @@ class SapOpenSqlBuilder extends MySqlBuilder
             [
                 ' LIMIT ',
                 '"',
-                '--'
+                //'--'
             ], [
                 ' UP TO ',
                 '',
-                '--"'
+                //'--"'
             ], 
             $query
         );
         
+        // Remove comments as they cause strange errors when SQL is copied into eclipse
+        // TODO find a way to use comments - otherwise it's hard to understand the query!
+        $query = preg_replace('/--.*/', "", $query);
+        
         // Add spaces to brackets
         $query = preg_replace('/\(([^ ])/', "( $1", $query);
+        $query = preg_replace('/([^ ])\)/', "$1 )", $query);
+        // The above does not replace )) with ) ) for some reason: repeating helps.
+        $query = preg_replace('/([^ ])\)/', "$1 )", $query);
         $query = preg_replace('/([^ ])\)/', "$1 )", $query);
         
         return $query;
@@ -115,8 +127,12 @@ class SapOpenSqlBuilder extends MySqlBuilder
                 $output = $subject . " LIKE '%" . $this->prepareWhereValue($value, $data_type) . "%'";
                 break;
             default:
-                return parent::buildSqlWhereComparator($subject, $comparator, $value, $data_type);
+                $output = parent::buildSqlWhereComparator($subject, $comparator, $value, $data_type);
         }
+        
+        // Add line breaks to IN statements (to avoid more than 255 characters per line)
+        $output = str_replace("','", "',\n'", $output);
+        
         return $output;
     }
     
@@ -129,7 +145,7 @@ class SapOpenSqlBuilder extends MySqlBuilder
     {
         // IDEA some data type specific procession here
         if ($data_type instanceof DateDataType) {
-            $value = $data_type::cast($value);
+            $value = $data_type->parse($value);
             return "'" . str_replace('-', '', $value) . "'";
         } 
         return parent::prepareWhereValue($value, $data_type, $sql_data_type);
@@ -143,9 +159,77 @@ class SapOpenSqlBuilder extends MySqlBuilder
     protected function prepareInputValue($value, DataTypeInterface $data_type, $sql_data_type = NULL)
     {
         if ($data_type instanceof DateDataType) {
-            $value = $data_type::cast($value);
+            $value = $data_type->parse($value);
             return "'" . str_replace('-', '', $value) . "'";
         }
         return parent::prepareWhereValue($value, $data_type, $sql_data_type);
+    }
+    
+    /**
+     * Adds a wrapper to a select statement, that should take care of the returned value if the statement
+     * itself returns null (like IFNULL(), NVL() or COALESCE() depending on the SQL dialect).
+     *
+     * @param string $select_statement            
+     * @param string $value_if_null            
+     * @return string
+     */
+    protected function buildSqlSelectNullCheck($select_statement, $value_if_null)
+    {
+        if (StringDataType::startsWith($select_statement, 'COUNT(') === true) {
+            return $select_statement;
+        }
+        return 'COALESCE(' . $select_statement . ', ' . (is_numeric($value_if_null) ? $value_if_null : "'" . $value_if_null . "'") . ')';
+    } 
+    
+    /**
+     * The OpenSQL builder cannot read attributes from reverse relations because OpenSQL
+     * does not support subqueries in SELECT clauses - only in WHERE (as of 7.50).
+     * 
+     * Since reversly related attributes cannot be read, they will be not part of
+     * the main query and will produce subqueries on data sheet level (subsheets)
+     * with WHERE IN and GROUP BY. The results will then be joined in-memory. 
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\QueryBuilders\AbstractSqlBuilder::canReadAttribute()
+     */
+    public function canReadAttribute(MetaAttributeInterface $attribute) : bool
+    {
+        $sameSource = parent::canReadAttribute($attribute);
+        if ($sameSource === false) {
+            return false;
+        }
+        
+        $relPath = $attribute->getRelationPath();
+        if ($relPath->isEmpty() === false) {
+            foreach ($relPath->getRelations() as $rel) {
+                if ($rel->isReverseRelation() === true) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\QueryBuilders\AbstractSqlBuilder::buildSqlGroupByExpression()
+     */
+    protected function buildSqlGroupByExpression(QueryPartAttribute $qpart, $sql, AggregatorInterface $aggregator){
+        $function_name = $aggregator->getFunction()->getValue();
+        
+        switch ($function_name) {
+            case AggregatorFunctionsDataType::COUNT:
+                // For some reason COUNT(col) produces a grammar error
+                return 'COUNT(*)';
+            default:
+                return parent::buildSqlGroupByExpression($qpart, $sql, $aggregator);
+        }
+    }
+    
+    protected function isEnrichmentAllowed() : bool
+    {
+        return false;
     }
 }
