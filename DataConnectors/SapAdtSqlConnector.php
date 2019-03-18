d343332161999b2ec55d92de7f6e93ed57ed517c
Andrej Kabachnik <andrej_ka@gmx.de>
2019-03-18 10:50:26 +0100

    FIX 401-error when trying to fetch the CSRF-token for web services

--- DataConnectors/SapAdtSqlConnector.php
+++ DataConnectors/SapAdtSqlConnector.php
@@ -68,21 +68,42 @@ class SapAdtSqlConnector extends HttpConnector implements SqlDataConnectorInterf
         return $this;
     }
     
-    protected function refreshCsrfToken() : string
+    /**
+     * 
+     * @param bool $retryOnError
+     * @throws RequestException
+     * @throws DataConnectionFailedError
+     * @return string
+     */
+    protected function refreshCsrfToken(bool $retryOnError = true) : string
     {
         $token = null;
         try {
             $response = $this->getClient()->get('freestyle',['headers' => ['X-CSRF-Token' => 'Fetch']]);
         } catch (RequestException $e) {
             $response = $e->getResponse();
+            // If there was an error, but there is no response (i.e. the error occurred before 
+            // the response was received), just rethrow the exception.
             if (! $response) {
                 throw $e;
             }
             $token = $response->getHeader('X-CSRF-Token')[0];
         }
         
+        // Sometimes, SAP returns a 401 error although the authentication is performed, so the same
+        // request works perfectly well the second time. Just retry to see if this works.
+        // TODO why does the 401 come in the first place? Perhaps it has something to do with
+        // the session expiring...
+        if (! $token && $retryOnError === true && $response && $response->getStatusCode() == 401) {
+            try {
+                $token = $this->refreshCsrfToken(false);
+            } catch (\Throwable $re) {
+                $this->getWorkbench()->getLogger()->logException(new DataConnectionFailedError($this, 'Retry fetch CSRF token failed: ' . $this->getErrorText($response), null, $re));
+            }
+        }
+        
         if (! $token) {
-            throw new DataConnectionFailedError($this, 'Cannot fetch CSRF token: ' . $this->getErrorText($response), null, $e);
+            throw new DataConnectionFailedError($this, 'Cannot fetch CSRF token: ' . $this->getErrorText($response) . '. See logs for more details!', null, $e);
         }
         
         $this->setCsrfToken($token);
