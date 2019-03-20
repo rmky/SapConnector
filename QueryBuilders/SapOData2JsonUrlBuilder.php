6519142a4847cd148ea6256bd06bff43a98c565f
Andrej Kabachnik <andrej_ka@gmx.de>
2019-03-20 12:44:04 +0100

    FIX error in OData filters with substringof() eq true

--- QueryBuilders/SapOData2JsonUrlBuilder.php
+++ QueryBuilders/SapOData2JsonUrlBuilder.php
@@ -11,5 +11,24 @@ use exface\UrlDataConnector\QueryBuilders\OData2JsonUrlBuilder;
  */
 class SapOData2JsonUrlBuilder extends OData2JsonUrlBuilder
 {
-    
+    /**
+     * 
+     * {@inheritdoc}
+     * @see OData2JsonUrlBuilder::buildUrlFilterPredicate
+     */
+    protected function buildUrlFilterPredicate(QueryPartFilter $qpart, string $property, string $escapedValue) : string
+    {
+        $comp = $qpart->getComparator();
+        switch ($comp) {
+            case EXF_COMPARATOR_IS:
+                // SAP NetWeaver produces a 500-error on substringof() eq true - need to remove the "eq true".
+                if ($qpart->getDataType() instanceof NumberDataType) {
+                    return parent::buildUrlFilterPredicate($qpart, $property, $escapedValue);
+                } else {
+                    return "substringof({$escapedValue}, {$property})";
+                } 
+            default:
+                return parent::buildUrlFilterPredicate($qpart, $property, $escapedValue);
+        }
+    }
 }
