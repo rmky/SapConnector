8c736cc3f162383138508182f91b0520bedcace6
aka <andrej.kabachnik@salt-solutions.de>
2019-04-04 20:59:02 +0200

    FIX date handling in SAP OData2

--- QueryBuilders/SapOData2JsonUrlBuilder.php
+++ QueryBuilders/SapOData2JsonUrlBuilder.php
@@ -3,6 +3,9 @@ namespace exface\SapConnector\QueryBuilders;
 
 use exface\UrlDataConnector\QueryBuilders\OData2JsonUrlBuilder;
 use exface\Core\CommonLogic\QueryBuilder\QueryPartFilter;
+use exface\UrlDataConnector\Psr7DataQuery;
+use exface\Core\DataTypes\DateDataType;
+use exface\Core\DataTypes\StringDataType;
 
 /**
  * Query builder for SAP oData services in JSON format.
@@ -31,5 +34,28 @@ class SapOData2JsonUrlBuilder extends OData2JsonUrlBuilder
             default:
                 return parent::buildUrlFilterPredicate($qpart, $property, $escapedValue);
         }
+    }
+    
+    protected function buildResultRows($parsed_data, Psr7DataQuery $query)
+    {
+        $rows = parent::buildResultRows($parsed_data, $query);
+        
+        foreach ($this->getAttributes() as $qpart) {
+            if ($qpart->getDataType() instanceof DateDataType) {
+                $dataType = $qpart->getDataType();
+                foreach ($rows as $rowNr => $row) {
+                    $val = $row[$qpart->getDataAddress()];
+                    if (StringDataType::startsWith($val, '/Date(')) {
+                        $mil = substr($val, 6, -2);
+                        $seconds = $mil / 1000;
+                        $newVal = $dataType->parse($seconds);
+                        $rows[$rowNr][$qpart->getDataAddress()] = $newVal;
+                    }
+                    
+                }
+            }
+        }
+        
+        return $rows;
     }
 }
