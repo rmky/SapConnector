d01e85ec8ca2bd11d451ecb0084e0986362ea0b1
aka <andrej.kabachnik@salt-solutions.de>
2019-04-04 20:58:36 +0200

    FIX support for CSRF-tokens in SapODataConnector

--- DataConnectors/SapOData2Connector.php
+++ DataConnectors/SapOData2Connector.php
@@ -4,6 +4,9 @@ namespace exface\SapConnector\DataConnectors;
 use exface\UrlDataConnector\DataConnectors\OData2Connector;
 use exface\SapConnector\ModelBuilders\SapOData2ModelBuilder;
 use exface\SapConnector\DataConnectors\Traits\CsrfTokenTrait;
+use exface\Core\Interfaces\DataSources\DataQueryInterface;
+use exface\UrlDataConnector\Psr7DataQuery;
+use exface\Core\Exceptions\DataSources\DataConnectionQueryTypeError;
 
 /**
  * HTTP data connector for SAP oData 2.0 services.
@@ -23,5 +26,25 @@ class SapOData2Connector extends OData2Connector
     public function getModelBuilder()
     {
         return new SapOData2ModelBuilder($this);
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\UrlDataConnector\DataConnectors\HttpConnector::performQuery()
+     */
+    protected function performQuery(DataQueryInterface $query)
+    {
+        if (! ($query instanceof Psr7DataQuery)) {
+            throw new DataConnectionQueryTypeError($this, 'Connector "' . $this->getAliasWithNamespace() . '" expects a Psr7DataQuery as input, "' . get_class($query) . '" given instead!');
+        }
+        /* @var $query \exface\UrlDataConnector\Psr7DataQuery */
+            
+        $request = $query->getRequest();
+        if ($request->getMethod() !== 'GET' && $request->getMethod() !== 'OPTIONS') {
+            $query->setRequest($request->withHeader('X-CSRF-Token', $this->getCsrfToken()));
+        }
+        
+        return parent::performQuery($query);
     }
 }
